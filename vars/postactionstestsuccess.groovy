4f667b2565728400e1ba6fd18f7330aad5771a90
Katerina Koutsonikoli <magikat@protonmail.com>
2019-05-03 17:17:05 +0300

    remove test files

--- vars/postactionstestsuccess.groovy
+++
@@ -1,9 +0,0 @@
-def call() {
-
-    //build job: 'test-registry', propagate: false, wait: false, parameters: [[$class: 'StringParameterValue', name: 'dockerImage', value: "${env.dockerImage}"]]
-    script {
-        if ("${env.projectBranch}" == 'testakis') {
-            build job: 'test-trigger', propagate: false, wait: false, parameters: [[$class: 'StringParameterValue', name: 'projectArtifacts', value: "${env.JOB_NAME}"]]
-        }
-    }
-}
