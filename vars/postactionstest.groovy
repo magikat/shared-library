dff8e5d4e4170368fba4bd0d863a78d2e393d9ea
Katerina Koutsonikoli <magikat@protonmail.com>
2019-05-03 16:52:41 +0300

    Update postactionstest.groovy

--- vars/postactionstest.groovy
+++ vars/postactionstest.groovy
@@ -1,17 +1,6 @@
 def call() {
 
-    post {
-        always {
-            archiveArtifacts artifacts: 'artifactsdir/*', onlyIfSuccessful: true
-            slackNotifier(currentBuild.currentResult)
-        }
-        success {
-            //build job: 'test-registry', propagate: false, wait: false, parameters: [[$class: 'StringParameterValue', name: 'dockerImage', value: "${env.dockerImage}"]]
-            script {
-                if ("${env.projectBranch}" == 'testakis') {
-                    build job: 'test-trigger', propagate: false, wait: false, parameters: [[$class: 'StringParameterValue', name: 'projectArtifacts', value: "${env.JOB_NAME}"]]
-                }
-            }   
-        }
-    }
+    archiveArtifacts artifacts: 'artifactsdir/*', onlyIfSuccessful: true
+    slackNotifier(currentBuild.currentResult)
+    
 }
