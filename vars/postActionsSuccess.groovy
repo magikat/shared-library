5ec09f19f0beea1416d95080fb7209b6cc0f5054
Katerina Koutsonikoli <magikat@protonmail.com>
2019-05-03 17:21:05 +0300

    Add shared library for success block of post actions

---
+++ vars/postActionsSuccess.groovy
@@ -0,0 +1,13 @@
+def call() {
+
+    String version = evaluate("version")
+
+    build job: 'docker_image', propagate: false, wait: false, parameters: [[$class: 'StringParameterValue', name: 'project', value: "${env.JOB_NAME}"],[$class: 'StringParameterValue', name: 'version', value: "${version}"],[$class: 'StringParameterValue', name: 'projectBranch', value: "${env.projectBranch}"]]
+    script {
+        if ("${env.projectBranch}" == 'master') {
+            archiveArtifacts artifacts: 'dist/**/*', onlyIfSuccessful: true
+            build job: 'upload_release_to_s3', propagate: false, wait: false, parameters: [[$class: 'StringParameterValue', name: 'projectArtifacts', value: "${env.JOB_NAME}"]]
+        }
+    }
+
+}
