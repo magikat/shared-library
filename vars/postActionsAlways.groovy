4ec8232a4bbab1a82f203c974349b599bef0802f
Katerina Koutsonikoli <magikat@protonmail.com>
2019-05-30 16:14:15 +0300

    Comment the deletion

--- vars/postActionsAlways.groovy
+++ vars/postActionsAlways.groovy
@@ -3,5 +3,5 @@ def call() {
     /* Use slackNotifier.groovy from shared library and provide current build result as parameter */   
     slackNotifier(currentBuild.currentResult)
     /* Delete workspace */
-    deleteDir()
+   /* deleteDir()*/
 }
