67d3c0ae5a646e1f2a663d9a5a49d1e67a4ae0e3
Katerina Koutsonikoli <magikat@protonmail.com>
2019-06-26 15:42:08 +0300

    Update process for releases of image
    Update process for releases that don't create archives but only images

--- vars/postActionsSuccess.groovy
+++ vars/postActionsSuccess.groovy
@@ -1,10 +1,15 @@
 def call() {
 
     String version = evaluate("version")
+    boolean image_only = evaluate("image_only")
+    
+    if ( image_only == null ) {
+        image_only = false
+    }
 
     build job: 'docker_image', propagate: false, wait: false, parameters: [[$class: 'StringParameterValue', name: 'project', value: "${env.JOB_NAME}"],[$class: 'StringParameterValue', name: 'version', value: "${version}"],[$class: 'StringParameterValue', name: 'projectBranch', value: "${env.projectBranch}"]]
     script {
-        if ("${env.projectBranch}" == 'master') {
+        if ("${env.projectBranch}" == 'master' && image_only == false) {
             archiveArtifacts artifacts: 'dist/**/*', onlyIfSuccessful: true
             build job: 'upload_release_to_s3', propagate: false, wait: false, parameters: [[$class: 'StringParameterValue', name: 'projectArtifacts', value: "${env.JOB_NAME}"]]
         }
